e9d0b1b6d8ebd6478c95f16c0eb47e26fb1aa4ea
aseemcr07 <60442085+aseemcr07@users.noreply.github.com>
2021-09-14 19:44:06 +0530

    Final app done

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-var formSelect = document.querySelector("form");
+var quizForm = document.querySelector(".quiz-form");
 var submitButton = document.querySelector(".submit-button");
-var output = document.querySelector(".output");
+var outputDiv = document.querySelector(".output");
 
 const correctAnswers = ['90°', 'Right-angled', 'One right angle', '12, 16, 20', 'Equilateral triangles', '85°', '40°', 'a + b + c', 'Zero', '45°'];
+
+function calculateScore(){
+    var score = 0;
+    var index = 0;
+    const data = new FormData(quizForm);
+    for(var pair of data.entries()){
+        
+        if(pair[1] === correctAnswers[index]){
+            console.log(pair[0] + ": " + pair[1] + " - Correct");
+            score++;
+        }
+        else{
+            console.log(pair[0] + ": " + pair[1] + " - Wrong");
+        }
+        index++;
+    }
+    console.log("Score: ", score);
+    outputDiv.innerHTML = "Your score: " + score;
+}
+
+submitButton.addEventListener("click", calculateScore);
