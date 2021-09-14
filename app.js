3d522d55589a2f05e0aba9a46a50bf2e4d465a72
aseemcr07 <60442085+aseemcr07@users.noreply.github.com>
2021-09-14 15:22:49 +0530

    Added correct answers

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ var formSelect = document.querySelector("form");
 var submitButton = document.querySelector(".submit-button");
 var output = document.querySelector(".output");
 
+const correctAnswers = ['90°', 'Right-angled', 'One right angle', '12, 16, 20', 'Equilateral triangles', '85°', '40°', 'a + b + c', 'Zero', '45°'];
