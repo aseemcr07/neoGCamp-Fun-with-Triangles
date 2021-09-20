0d651001307c64156abc67706706c3cc1039e8ac
aseemcr07 <60442085+aseemcr07@users.noreply.github.com>
2021-09-20 23:36:25 +0530

    Final app done

--- hypotenuse.js
+++ hypotenuse.js
@@ -2,3 +2,13 @@ var sideLengths = document.querySelectorAll(".input-box");
 var calculateBtn = document.querySelector(".calculate-button");
 var outputDiv = document.querySelector(".output");
 
+function calculateHypotenuse(){
+    var base = Number(sideLengths[0].value);
+    var height = Number(sideLengths[1].value);
+
+    var hypotenuse = Math.sqrt((base*base) + (height*height));
+    
+    outputDiv.innerHTML = "Length of hypotenuse = " + hypotenuse;
+}
+
+calculateBtn.addEventListener("click", calculateHypotenuse);
