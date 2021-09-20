e938bc136244cc085b5bb825b5538b49fc2821ae
aseemcr07 <60442085+aseemcr07@users.noreply.github.com>
2021-09-20 19:52:44 +0530

    Final app done

--- is_triangle.js
+++ is_triangle.js
@@ -4,8 +4,8 @@ var outputDiv = document.querySelector(".output");
 
 function calculateSumOfAngles(listOfAngles){
     var sum = 0;
-    for(var angle of listOfAngles){
-        sum = sum + Number(angle);
+    for(var i=0; i<listOfAngles.length; i++){
+        sum = sum + Number(angles[i].value);
     }
     return sum;
 }
@@ -13,10 +13,10 @@ function calculateSumOfAngles(listOfAngles){
 function isTriangle(){
     var sumOfAngles = calculateSumOfAngles(angles);
     if(sumOfAngles === 180){
-        outputDiv.innerHTML = "Yes! It forms a triangle!";
+        outputDiv.innerHTML = "Yes! It forms a triangle 🥳 🥳";
     }
     else{
-        outputDiv.innerHTML = "They don't form a triangle";
+        outputDiv.innerHTML = "They don't form a triangle 😕";
     }
 }
 
